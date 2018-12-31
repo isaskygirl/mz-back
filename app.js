ba82a5f5a13ae21ace93d52f51274d178081fd22
Elmvx <elvmx47@aliyun.com>
2018-12-31 13:39:57 +0800

    film 列表

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 
 const banner = require('./routes/banner');
 const city = require('./routes/city');
+const film = require('./routes/film');
 
 app.use('/banner', banner);
 app.use('/city', city);
+app.use('/film', film);
 
 app.listen(9090);
 console.log('服务启动成功');
